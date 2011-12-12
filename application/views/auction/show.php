f4d4955f8ab378f86fef56cd568dac8a745d5956
Mike Gauthier <michael.gauthier@gmail.com>
2011-12-11 21:38:34 -0800

    small tweak

--- application/views/auction/show.php
+++ application/views/auction/show.php
@@ -1,18 +1,11 @@
-<html>
-	<head>
-		<title>Auctions</title>
-		<?= self::stylesheet('style') ?>
-		<?= self::javascript('jquery-1.7.1.min') ?>
-		<?= self::javascript('app') ?>
-	</head>
-	<body>
-
-	<?= self::image('pic.jpg', array("id" => "imgID", "width" => "100px", "class" => "small_pic")) ?>
-
-	This is the Auction List Page
-	<br>
-	<? self::partial('auction','show'); ?>
-	<br>
-	<? self::partial('user','default'); ?>
-	</body>
-</html>
+<?= self::image('pic.jpg', array("id" => "imgID", "width" => "100px", "class" => "small_pic")) ?>
+<br>
+This is the Auction List Page
+<br>
+<? 
+self::partial('auction','show'); 
+?>
+<br>
+<? 
+self::partial('user','default'); 
+?>	
