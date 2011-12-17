1def5979668acedcb08251200104db75dcab0769
Mike Gauthier <michael.gauthier@gmail.com>
2011-12-17 02:11:59 -0800

    small updates to list script

---
+++ scripts/list.php
@@ -0,0 +1,32 @@
+<?php
+require_once(getcwd()."/application/DiamondBase.php");
+
+$type = $argv[1] ? strtolower($argv[1]) : null;
+print $type ? "" : "No valid type entered\n";
+
+switch($type) {
+	case "all":
+		print "Models:\n";
+		_list("model");
+		print "\nControllers:\n";
+		_list("controller");
+		print "\nViews:\n";
+		_list("view");
+	break;
+	case "models":
+	case "controllers":
+	case "views":
+		print ucfirst($type).":\n";
+		_list(substr($type,0,count($type)-2));
+	break;
+	default:
+		print "Sorry, $type is not a valid type\n";
+	break;
+}
+
+function _list($type) {
+	$list = scandir("application/".$type."s");
+	foreach($list as $value) { 
+		print $value != "." && $value != ".." ? DiamondBase::fileToType($value,$type)."\n" : ""; 
+	}
+}
